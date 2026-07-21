0b3befa022a66bbe247ebc5b806ea69439f1faff
CStathakis95 <cstathakis@uniting.org>
2026-07-21 15:24:40 +1000

    Update distributor.js

--- js/distributor.js
+++ js/distributor.js
@@ -1,1 +1,24 @@
 // Distributor detection will go here
+function findDistributor(address) {
+
+    address = address.toLowerCase();
+
+    if (
+        address.includes("sydney") ||
+        address.includes("newcastle") ||
+        address.includes("central coast")
+    ) {
+        return "Ausgrid";
+    }
+
+    if (
+        address.includes("penrith") ||
+        address.includes("parramatta") ||
+        address.includes("wollongong")
+    ) {
+        return "Endeavour Energy";
+    }
+
+    return "Essential Energy";
+
+}
