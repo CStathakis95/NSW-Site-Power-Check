faaccdabe3798b1462982b78c79bb9f999690fb7
CStathakis95 <cstathakis@uniting.org>
2026-07-21 16:02:13 +1000

    Update outage.js

--- js/outage.js
+++ js/outage.js
@@ -1,1 +1,23 @@
 // Outage checking will go here
+async function checkOutage(distributor, address) {
+
+    if (distributor === "Endeavour Energy") {
+        return await checkEndeavour(address);
+    }
+
+    return {
+        status: "No live outage check available yet",
+        details: "This distributor is coming soon."
+    };
+
+}
+
+
+async function checkEndeavour(address) {
+
+    return {
+        status: "Testing connection",
+        details: "Endeavour Energy outage lookup will be connected here."
+    };
+
+}
