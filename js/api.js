b8bed019c899d4e604284e3f40c8cc1800bbf3db
CStathakis95 <cstathakis@uniting.org>
2026-07-23 11:53:41 +1000

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,33 @@
+async function getOutage(address) {
+
+    try {
+
+        const response = await fetch(`${CONFIG.API_URL}/check`, {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                address
+            })
+
+        });
+
+        return await response.json();
+
+    } catch (error) {
+
+        return {
+
+            status: "Error",
+
+            details: "Unable to contact outage service."
+
+        };
+
+    }
+
+}
